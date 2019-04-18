8e5a0b3710ff95898ed91473f7ce31ffad612a9c
Corey Doss <cjdw94@mail.umkc.edu>
2019-04-18 18:02:03 -0500

    Added frontend example w/ upload functionality

---
+++ ex_ang_upload_frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UploadModule } from './upload/upload.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+	UploadModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
