826b04655c2ff5c699a47bbc365696d04606282d
a190884810 <39787887+a190884810@users.noreply.github.com>
2019-04-19 14:50:25 -0500

    Add files via upload

---
+++ models/MobileNet.py
@@ -0,0 +1,63 @@
+import sys
+import os
+import numpy as np
+
+# Keras
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+
+from keras.applications.mobilenet import MobileNet, preprocess_input, decode_predictions
+model = MobileNet(weights='imagenet')
+print('The model MobileNet loaded')
+print('Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img) # Preprocessing the image
+    x = np.expand_dims(x, axis=0) # x = np.true_divide(x, 255)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    #x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(preprocess_input(x))
+    return preds
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions(preds, top=5)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
